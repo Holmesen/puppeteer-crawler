5ca1ed3a926b73c9fdd30dc2a0dfa5537de333ba
Holmesen <1315654996@qq.com>
2023-08-15 23:17:17 +0800

    navi.js

--- navi.js
+++ navi.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -7,7 +8,7 @@ const puppeteer = require('puppeteer');
   });
 
   // await getTitle(browser);
-  await gotoMenuPage(browser, {link: 'https://www.colabug.com/favorites/image-website/'})
+  await gotoMenuPage(browser, { link: 'https://www.colabug.com/favorites/image-website/' });
 
   // await browser.close();
 })();
@@ -33,7 +34,7 @@ async function getTitle(browser) {
     if (tabsN) {
       let subTList = [];
       for (const tab of tabsN) {
-        const menus = []
+        const menus = [];
         const menuN = await tab.$('.slider_menu');
         if (menuN) {
           const navNList = await menuN.$$('.nav-item');
@@ -42,15 +43,15 @@ async function getTitle(browser) {
               const menu = await navN.$eval('a', (node) => {
                 return {
                   title: node.innerText?.trim(),
-                  link: node.getAttribute('data-link')?.trim()
-                }
+                  link: node.getAttribute('data-link')?.trim(),
+                };
               });
               console.log(menu);
-              menus.push(menu)
+              menus.push(menu);
             }
           }
         }
-        subTList.push(menus)
+        subTList.push(menus);
         console.log('----------------');
       }
       subTList.forEach((it) => {
@@ -60,42 +61,75 @@ async function getTitle(browser) {
   }
 }
 
-async function gotoMenuPage(browser, menu) {
+async function gotoMenuPage(browser, menu, pageNum = 1, autoNext = false) {
   if (menu.link) {
-    const page = await browser.newPage()
-    await page.setViewport({ width: 1080, height: 1024 })
-    await page.goto(menu.link, {timeout: 60 * 1000})
-    const ctx = await page.$('.content-layout')
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1080, height: 1024 });
+    await page.goto(menu.link, { timeout: 60 * 1000 });
+    const ctx = await page.$('.content-layout');
     if (ctx) {
-      const row = await ctx.$('.row')
+      const row = await ctx.$('.row');
       if (row) {
-        const list = await row.$$('.url-card')
-        list && await handlenNaviList(list)
+        const list = await row.$$('.url-card');
+        list && (await handlenNaviList(list, pageNum));
       }
-      const page = await ctx.$('.posts-nav')
+      if (!autoNext) {
+        console.log('第1页');
+        const pageN = await ctx.$('.posts-nav');
+        if (pageN) {
+          const aNList = await pageN.$$eval('.page-numbers', (nodes) => nodes.map((n) => n.innerText));
+          console.log(aNList);
+          if (aNList && aNList.length > 0) {
+            await handlePageList(aNList.at(-1), browser, menu);
+          }
+        }
+      }
     }
   }
 }
 
-async function handlenNaviList(list) {
-  const navList = []
+async function handlenNaviList(list, pageNum = 1) {
+  const navList = [];
   for await (const nav of list) {
-    const obj1 = await nav.$eval('a.card', el => {
+    const obj1 = await nav.$eval('a.card', (el) => {
       return {
         link: el.getAttribute('href'),
-        intro: el.getAttribute('data-original-title')
-      }
-    })
-    
-    const obj2 = await nav.$eval('img.unfancybox', el => {
+        intro: el.getAttribute('data-original-title'),
+      };
+    });
+
+    const obj2 = await nav.$eval('img.unfancybox', (el) => {
       return {
         img: el.getAttribute('src'),
-        title: el.getAttribute('alt')
-      }
-    })
+        title: el.getAttribute('alt'),
+      };
+    });
 
-    const obj = {...obj1, ...obj2}
+    const obj = { ...obj1, ...obj2 };
     console.log(obj);
-    navList.push(obj)
+    navList.push(obj);
   }
+  await outputFile(navList, pageNum);
+}
+
+async function handlePageList(num, browser, menu) {
+  for (let i = 2; i <= num; i++) {
+    const url = `${menu.link}/page/${i}/`;
+    menu.link = url;
+    console.log('*******************');
+    console.log(`第${i}页`);
+    await gotoMenuPage(browser, menu, i, true);
+  }
+}
+
+async function outputFile(result, pageNum) {
+  const resultString = JSON.stringify(result, null, 2);
+  // const output = `module.exports = ${resultString};`;
+  fs.writeFile(`data-${pageNum}.json`, resultString, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    } else {
+      console.log('Output file created successfully.');
+    }
+  });
 }
