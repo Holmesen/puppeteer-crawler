42cbd1d70f590fad236d8b10e9bba30e547d3d71
Holmesen <1315654996@qq.com>
2023-08-16 22:44:13 +0800

    navi.js

--- navi.js
+++ navi.js
@@ -6,13 +6,11 @@ const fs = require('fs');
   //   executablePath: 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe',
   //   headless: false,
   // });
-
   // await getTitle(browser);
   // await gotoMenuPage(browser, { link: 'https://www.colabug.com/favorites/image-website/' });
-
   // const page = await browser.newPage();
   // await handleDetail(page, 'https://www.colabug.com/sites/14385/')
-
+  // await handlePageList(browser);
   // await browser.close();
 })();
 
@@ -64,34 +62,8 @@ async function getTitle(browser) {
   }
 }
 
-async function gotoMenuPage(browser, menu, pageNum = 1, autoNext = false) {
-  if (menu.link) {
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1080, height: 1024 });
-    await page.goto(menu.link, { timeout: 60 * 1000 });
-    const ctx = await page.$('.content-layout');
-    if (ctx) {
-      const row = await ctx.$('.row');
-      if (row) {
-        const list = await row.$$('.url-card');
-        list && (await handlenNaviList(list, pageNum));
-      }
-      if (!autoNext) {
-        console.log('第1页');
-        const pageN = await ctx.$('.posts-nav');
-        if (pageN) {
-          const aNList = await pageN.$$eval('.page-numbers', (nodes) => nodes.map((n) => n.innerText));
-          console.log(aNList);
-          if (aNList && aNList.length > 0) {
-            await handlePageList(aNList.at(-1), browser, menu);
-          }
-        }
-      }
-    }
-  }
-}
-
-async function handlenNaviList(list, pageNum = 1) {
+// 处理分类页的导航信息2
+async function handleNaviList(list) {
   const navList = [];
   for await (const nav of list) {
     const obj1 = await nav.$eval('a.card', (el) => {
@@ -112,19 +84,43 @@ async function handlenNaviList(list, pageNum = 1) {
     console.log(obj);
     navList.push(obj);
   }
-  await outputFile(navList, pageNum);
+  console.log('结束↑↑↑↑↑↑↑↑↑↑');
+  // await outputFile(navList, pageNum);
 }
 
-async function handlePageList(num, browser, menu) {
-  for (let i = 2; i <= num; i++) {
-    const url = `${menu.link}/page/${i}/`;
-    menu.link = url;
+// 分类页
+async function handlePageList(browser) {
+  const baseUrl = 'https://www.colabug.com/sitetag/ai-writing-tools/';
+  const pageCount = 2;
+  for (let i = 1; i <= pageCount; i++) {
+    let url = baseUrl;
+    if (i > 1) {
+      url = `${baseUrl}page/${i}/`;
+    }
     console.log('*******************');
-    console.log(`第${i}页`);
-    await gotoMenuPage(browser, menu, i, true);
+    await wait(10);
+    console.log(`第${i}页开始↓↓↓↓↓↓↓↓↓↓↓`);
+    await handleCateList(url, browser);
+  }
+}
+// 处理分类页的所有导航信息
+async function handleCateList(url, broswer) {
+  console.log('url: ', url);
+  if (url) {
+    const page = await broswer.newPage();
+    await page.goto(url, { timeout: 60 * 1000 });
+    const ctx = await page.$('.content-layout');
+    if (ctx) {
+      const row = await ctx.$('.row');
+      if (row) {
+        const list = await row.$$('.url-card');
+        list && (await handleNaviList(list));
+      }
+    }
   }
 }
 
+// 输出到文件
 async function outputFile(result, pageNum) {
   const resultString = JSON.stringify(result, null, 2);
   // const output = `module.exports = ${resultString};`;
@@ -139,32 +135,33 @@ async function outputFile(result, pageNum) {
 
 // 导航详情
 async function handleDetail(page, link) {
-  await page.goto(link, {timeout: 60 * 1000})
-  const ctx = await page.$('#content')
+  await page.goto(link, { timeout: 60 * 1000 });
+  const ctx = await page.$('#content');
   if (ctx) {
-    const siteBody = await ctx.$('.site-body')
+    const siteBody = await ctx.$('.site-body');
     if (siteBody) {
-      const siteName = await siteBody.$eval('.site-name', node => node.innerText)
+      const siteName = await siteBody.$eval('.site-name', (node) => node.innerText);
       console.log(siteName);
-      const tags = await siteBody.$$eval('.mr-2', nodes => nodes.map(n => n.innerText))
+      const tags = await siteBody.$$eval('.mr-2', (nodes) => nodes.map((n) => n.innerText));
       console.log(tags);
     }
 
-    const ctxL = await ctx.$('.content-layout')
+    const ctxL = await ctx.$('.content-layout');
     if (ctxL) {
-      const siteCtx = await ctxL.$('.site-content')
+      const siteCtx = await ctxL.$('.site-content');
       if (siteCtx) {
-        const intro = await siteCtx.$eval('.panel-body', node => node.innerText)
+        const intro = await siteCtx.$eval('.panel-body', (node) => node.innerText);
         console.log(intro);
       }
     }
   }
 }
 
-async function wait(s=0) {
+// 等待函数
+async function wait(s = 0) {
   await new Promise((resolve) => {
     setTimeout(() => {
-      resolve(1)
-    }, s*1000)
-  })
+      resolve(1);
+    }, s * 1000);
+  });
 }
